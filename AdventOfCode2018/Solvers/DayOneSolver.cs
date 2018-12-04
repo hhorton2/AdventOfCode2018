3a5418695c48174ae6e072836aadcdfcc039928e
Hunter Horton <hhorton@atlasrfid.com>
2018-12-03 19:49:36 -0600

    part two using just dataflow

--- AdventOfCode2018/Solvers/DayOneSolver.cs
+++ AdventOfCode2018/Solvers/DayOneSolver.cs
@@ -42,49 +42,40 @@ namespace AdventOfCode2018.Solvers
             var readBlock = CommonBlockFactory.GetLinesFromTextFile();
             var flattenBlock =
                 new TransformManyBlock<string[], string>(lines => lines, CommonBlockFactory.OrderedBlockOptions);
+            var parseBuffer = new BufferBlock<string>(CommonBlockFactory.OrderedBlockOptions);
             var parseBlock =
                 new TransformBlock<string, int>(stringValue => int.Parse(stringValue),
                     CommonBlockFactory.OrderedBlockOptions);
             var sum = 0;
             var dictionary = new Dictionary<int, int>();
             dictionary.TryAdd(0, 0);
-            var list = new List<int>();
-            var found = false;
-            var collectBlock = new ActionBlock<int>(num =>
+            var sumBuffer = new BufferBlock<int>(CommonBlockFactory.OrderedBlockOptions);
+            bool found;
+            var sumBlock = new ActionBlock<int>(num =>
                 {
                     sum += num;
                     found = !dictionary.TryAdd(sum, sum);
                     if (!found)
                     {
-                        list.Add(num);
+                        sumBuffer.Post(num);
+                    }
+                    else
+                    {
+                        sumBuffer.Complete();
                     }
                 },
-                new ExecutionDataflowBlockOptions{MaxDegreeOfParallelism = 1, MaxMessagesPerTask = 1});
+                new ExecutionDataflowBlockOptions {MaxDegreeOfParallelism = 1, MaxMessagesPerTask = 1});
 
             var linkOptions = new DataflowLinkOptions {PropagateCompletion = true};
             readBlock.LinkTo(flattenBlock, linkOptions);
-            flattenBlock.LinkTo(parseBlock, linkOptions);
-            parseBlock.LinkTo(collectBlock, linkOptions);
+            flattenBlock.LinkTo(parseBuffer, linkOptions);
+            parseBuffer.LinkTo(parseBlock, linkOptions);
+            parseBlock.LinkTo(sumBlock);
+            sumBuffer.LinkTo(sumBlock, linkOptions);
             readBlock.Post(
                 $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\InputFiles\\day1\\partOne.txt");
             readBlock.Complete();
-            collectBlock.Completion.Wait();
-            if (!found)
-            {
-                while (!found)
-                {
-                    foreach (var number in list)
-                    {
-                        sum += number;
-                        found = !dictionary.TryAdd(sum, sum);
-                        if (found)
-                        {
-                            break;
-                        }
-                    }
-                }
-            }
-
+            sumBlock.Completion.Wait();
             Console.WriteLine(sum);
         }
     }
